1a0125d92721858a579eadffe08f5f966c221978
pcprashant <pcprashant3@gmail.com>
2022-07-23 13:56:23 -0500

    movies-app-commit-1

---
+++ script.js
@@ -0,0 +1,80 @@
+const apiKey =  '04c35731a5ee918f014970082a0088b1';
+const apiUrl = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1"
+
+
+const imgPath = "https://image.tmdb.org/t/p/w1280"
+const searchApi = 'https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query='
+
+
+const main = document.querySelector('main')
+const form = document.querySelector('#form')
+const search = document.querySelector('.search')
+
+getMovies(apiUrl);
+
+
+async function getMovies(url) {
+    const resp = await fetch(url);
+    const respData = await resp.json();
+
+    console.log(respData.results)
+    showMovies(respData.results)
+
+}
+
+function showMovies(movies) {
+    main.innerHTML = '';
+    movies.forEach(movie => {
+
+        const {poster_path, title, vote_average, overview} = movie;
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = `
+        <img
+            src="${imgPath + poster_path}" 
+            alt="${title}"
+         />
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class = '${getClassByRate(vote_average)}'>${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview:</h3>
+                ${overview}
+            </div>
+
+        `;
+
+        // const details = document.querySelector('.overview');
+
+        main.appendChild(movieEl);
+
+    })
+
+
+}
+
+
+function getClassByRate(vote) {
+    if(vote >= 8) {
+        return 'green';
+    } else if (vote >= 5 ) {
+        return 'orange'
+    } else {
+        return 'red'
+    }
+}
+
+
+form.addEventListener('submit', (e) => {
+
+    e.preventDefault()
+    const searchTerm = search.value;
+
+    if(searchTerm) {
+        getMovies(searchApi + searchTerm);
+        search.value = " ";
+    }
+
+})
+
